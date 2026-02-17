cce58ebbb1b5c304777f2ebc525996a2dba5c9cf
66010914022 <66010914022@msu.ac.th>
2026-02-17 17:50:26 +0700

    a

---
+++ a.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อดิเทพ จำเริญเจือ (อาร์ม)</title>
+</head>
+
+<body>
+<h1>66010914022 อดิเทพ จำเริญเจือ (อาร์ม)</h1>
+
+
+</body>
+</html>
